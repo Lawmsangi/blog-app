c73da3132c801063db74d64f354875b02431e42c
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-11-27 23:40:41 +0530

    Create model classes and methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,12 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+
+    after_create :update_post_comments_count
+
+    private
+
+    def update_comments_count
+        post.increment!(:comments_counter)
+    end
 end
