28ad6197c9ae6fe88553a7e8eeca811150f6b578
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-04 22:18:38 +0530

    Add test for controller

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'PostsController', type: :request do
+  let!(:user) { User.create(name: 'John Doe', photo: 'photo', bio: 'bio') }
+  let!(:post) { Post.create(title: 'title', text: 'text', author: user) }
+
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get user_posts_path(user)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders a correct template' do
+      get user_posts_path(user)
+      expect(response).to render_template(:index)
+    end
+
+    it 'returns response body with correct placeholder text' do
+      get user_posts_path(user)
+      expect(response.body).to include('Posts')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a success response' do
+      get user_post_path(user, post)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders a correct template' do
+      get user_post_path(user, post)
+      expect(response).to render_template(:show)
+    end
+
+    it 'returns response body with correct placeholder text' do
+      get user_post_path(user, post)
+      expect(response.body).to include('Post')
+    end
+  end
+end
