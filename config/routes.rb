0bd5480eb8559af2f0453de4cfa991f96a4ee2e6
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-06 12:47:15 +0530

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  get 'comments/new'
+  get 'comments/create'
+  resources :users do
+    resources :posts do
+      resources :comments
+      resources :likes
+    end
   end
   root 'users#index'
 end
