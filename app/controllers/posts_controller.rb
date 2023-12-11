297734ed3d85c91430a776e54cc02b7855080913
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-11 15:23:54 +0530

    Add n+1 solution

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
   before_action :set_user
 
   def index
     @posts = @user.posts
   end
 
-  def show; end
+  def show
+    @post = Post.includes(:comments).find(params[:id])
+  end
 
   def create
     @user = User.find(params[:user_id])
@@ -23,10 +24,6 @@ class PostsController < ApplicationController
   end
 
   private
-
-  def set_post
-    @post = Post.find(params[:id])
-  end
 
   def set_user
     @user = User.find(params[:user_id])
