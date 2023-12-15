4e0f84fd0766c2cb296bccec50a70fa0f93071f3
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-15 22:40:54 +0530

    Make valid json response

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user!
+
   def index
     post = Post.find(params[:post_id])
     @comments = post.comments
@@ -10,14 +11,16 @@ class Api::V1::CommentsController < ApplicationController
   def create
     user_id = params[:user_id]
     post_id = params[:post_id]
-    @comment = Comment.new(user_id:, post_id:, text: comment_params[:text])
+    @comment = Comment.new(user_id: user_id, post_id: post_id, text: comment_params[:text])
 
     if @comment.save
-      render json: 'Comment saved successfully!'
+      render json: { message: 'Comment saved successfully!' }
     else
-      render json: { error: 'Error creating comment' }
+      render json: { error: 'Error creating comment' }, status: :unprocessable_entity
     end
   end
+
+  private
 
   def comment_params
     params.require(:comment).permit(:text)
