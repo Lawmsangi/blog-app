84751e573c9dd56e57a66fbfe2ef19b31ee25736
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-15 21:51:38 +0530

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1' do
-      resources :posts, only: [:index] do
-        resources :comments, only: [:create, :index]
+      resources :users, only: [:index] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:create, :index]
+        end
       end
     end
   end
