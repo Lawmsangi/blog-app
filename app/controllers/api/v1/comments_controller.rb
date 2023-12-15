63d35c522fead679f925a60f44531f7806e66172
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-15 21:51:51 +0530

    Update controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
   def index
-    @comments = current_user.comments
+    post = Post.find(params[:post_id])
+    @comments = post.comments
     render json: @comments
   end
 
   def create
+    user_id = params[:user_id]
     post_id = params[:post_id]
-    @comment = Comment.new(user_id: current_user.id, post_id:, text: comment_params[:text])
+    @comment = Comment.new(user_id:, post_id:, text: comment_params[:text])
 
     if @comment.save
       render json: 'Comment saved successfully!'
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
   def index
-    @posts = current_user.posts
+    user_id = params[:user_id]
+    user = User.find(user_id)
+    @posts = user.posts
     render json: @posts
   end
 end
